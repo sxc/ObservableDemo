2e3a5380f0d0d421334c1596ab02c1f3313038fe
sxc <qiuyequ@icloud.com>
2020-12-23 11:27:54 +0800

    add counter and restart

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var timerData: TimerData = TimerData()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Timer count = \(timerData.timeCount)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                Button(action: resetCount) {
+                    Text("Reset Counter")
+                }
+            }
+        }
+    }
+    
+    func resetCount() {
+        timerData.resetCount()
     }
 }
 
