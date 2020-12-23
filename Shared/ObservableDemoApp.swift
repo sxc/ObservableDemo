8c1b645241d00cc0801b248d84c9315b191d356b
sxc <qiuyequ@icloud.com>
2020-12-23 11:21:03 +0800

    Initial Commit

---
+++ Shared/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
